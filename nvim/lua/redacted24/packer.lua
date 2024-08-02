278637c8305aa3a64a953aded3c2d7ea7d0fec1a
Qiuyu Huang <huangq4@outlook.com>
2024-08-01 23:37:24 -0400

    add: Primeagen config, no LSP yet

---
+++ nvim/lua/redacted24/packer.lua
@@ -0,0 +1,30 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+
+  use 'wbthomason/packer.nvim'
+  -- Simple plugins can be specified as strings
+  use 'rstacruz/vim-closer'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use({
+	  'catppuccin/nvim',
+	  as = 'catppuccin',
+	  config = function()
+		  vim.cmd('colorscheme catppuccin-mocha')
+	  end
+  })
+
+  use("nvim-treesitter/nvim-treesitter", {run= ':TSUpdate'})
+  use('nvim-treesitter/playground') 
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  
+end)
