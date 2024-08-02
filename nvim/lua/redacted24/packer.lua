b0fc370ce58659cfa51004c65604fc433de390c8
Qiuyu Huang <huangq4@outlook.com>
2024-08-02 14:09:00 -0400

    add: lualine plugin

--- nvim/lua/redacted24/packer.lua
+++ nvim/lua/redacted24/packer.lua
@@ -39,4 +39,9 @@ return require('packer').startup(function(use)
 	  }
   }
 
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
 end)
