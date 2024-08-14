5260b977e15c2eb6e237a4d2be91eb58d6d3a308
Qiuyu Huang <huangq4@outlook.com>
2024-08-13 22:29:09 -0400

    add: comments for autocmd file

--- nvim/lua/redacted24/autocmd.lua
+++ nvim/lua/redacted24/autocmd.lua
@@ -1,8 +1,10 @@
+-- On vim enter, remove kitty spacing
 vim.api.nvim_create_autocmd({'VimEnter'}, {
     pattern = {'*'},
     command = "!kitty @ set-spacing padding-left=0 padding-top=0 padding-right=0",
 })
 
+-- On vim exit, restore default kitty spacing
 vim.api.nvim_create_autocmd({'VimLeave'}, {
     pattern = {'*'},
     command = "!kitty @ set-spacing default",
