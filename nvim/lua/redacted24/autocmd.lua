1d8be3d1f9c9542b401b3cdc0a3c76d0f916fadf
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2024-10-24 15:00:10 -0400

    update

--- nvim/lua/redacted24/autocmd.lua
+++ nvim/lua/redacted24/autocmd.lua
@@ -1,13 +1,13 @@
 -- On vim enter, remove kitty spacing
-vim.api.nvim_create_autocmd({'VimEnter'}, {
-    pattern = {'*'},
-    command = "echo 'wtf'"
-    -- command = "kitten @ set-spacing padding-left=0 padding-top=0 padding-right=0",
-})
-
--- On vim exit, restore default kitty spacing
-vim.api.nvim_create_autocmd({'VimLeave'}, {
-
-    pattern = {'*'},
-    command = "kitten @ set-spacing default",
-})
+-- vim.api.nvim_create_autocmd({'VimEnter'}, {
+--     pattern = {'*'},
+--     command = "echo 'wtf'"
+--     -- command = "kitten @ set-spacing padding-left=0 padding-top=0 padding-right=0",
+-- })
+--
+-- -- On vim exit, restore default kitty spacing
+-- vim.api.nvim_create_autocmd({'VimLeave'}, {
+--
+--     pattern = {'*'},
+--     command = "kitten @ set-spacing default",
+-- })
