24b3abb13d057a3fc82576499d1df72fd6e48c69
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2026-04-03 17:21:30 -0400

    feat: telescope find file and grep code

--- dot_config/nvim/lua/redacted24/plugins/telescope.lua
+++ dot_config/nvim/lua/redacted24/plugins/telescope.lua
@@ -11,10 +11,11 @@ return {
         require('telescope').setup({})
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
-        end)
+        vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+        -- vim.keymap.set('n', '<leader>ps', function()
+        --     builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        -- end)
     end 
 }
 
